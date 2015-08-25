211fe1389d728e783df1e326d57045570e60c2c6
vevalentino24 <ve.febrian@gmail.com>
2015-08-25 23:26:21 +0700

    repo awal

---
+++ app/Http/routes.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+
+Route::get('/', 'TestController@index');
+
+Route::get('/hehe', function(){
+	$data['tasks'] = [
+        [
+            'name' => 'Design New Dashboard',
+            'progress' => '87',
+            'color' => 'danger'
+        ],
+        [
+            'name' => 'Create Home Page',
+            'progress' => '76',
+            'color' => 'warning'
+        ],
+        [
+            'name' => 'Some Other Task',
+            'progress' => '32',
+            'color' => 'success'
+        ],
+        [
+            'name' => 'Start Building Website',
+            'progress' => '56',
+            'color' => 'info'
+        ],
+        [
+            'name' => 'Develop an Awesome Algorithm',
+            'progress' => '10',
+            'color' => 'success'
+        ]
+    ];
+    return view('nganu')->with($data);
+});
